ce6613374e626aa1990874b7e8571a4c6bf9c495
MSA <crystalbluetech@gmail.com>
2023-10-31 01:08:35 +0100

    rails setup files ,generate root controller and define routes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
+// import "@hotwired/turbo-rails"
+// import "./controllers"
+
+import './react/src/index'
+import * as bootstrap from "bootstrap"
